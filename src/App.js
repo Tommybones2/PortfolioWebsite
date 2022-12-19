31eea502dfbe98a14db0a96abafaf296769fa897
Tommybones2 <tbonagu1@gmail.com>
2022-12-18 22:11:22 -0500

    ROUGH DRAFT

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import { Link, Routes, Route } from 'react-router-dom';
+import InvestmentsPage from './InvestmentsPage';
+import AboutPage from './AboutPage';
 
 function App() {
   return (
@@ -7,18 +10,33 @@ function App() {
       <header className="App-header">
         <div className="name-container">
           <h1>Thomas Bonagura</h1>
-          <div className="links">
-            <a href="https://github.com/your-github-username" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <div className="orb"></div>
+          <div className="orb"></div>
+          <div className="orb"></div>
+        </div>
+        <div className="links-container">
+          <div className="link-box" style={{ backgroundColor: 'lightblue' }}>
+            <a href="https://github.com/Tommybones2" target="_blank" rel="noopener noreferrer">GitHub</a>
+          </div>
+          <div className="link-box" style={{ backgroundColor: 'pink' }}>
+            <a href="https://path-to-resume" target="_blank" rel="noopener noreferrer">Resume</a>
+          </div>
+          <div className="link-box" style={{ backgroundColor: 'lightgreen' }}>
+            <a href="https://path-to-thisSourceCode" target="_blank" rel="noopener noreferrer">This Source Code</a>
           </div>
         </div>
       </header>
       <main>
         <nav>
-          <a href="/investments">Investments</a>
-          <a href="/about">About</a>
+          <Link to="/investments">Investments</Link>
+          <Link to="/about">About</Link>
         </nav>
         <div className="homepage-content">
-          {/* add your homepage content here */}
+          {/* Homepage content goes here */}
+          <Routes>
+            <Route path="/investments" element={<InvestmentsPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
         </div>
       </main>
     </div>
@@ -26,3 +44,4 @@ function App() {
 }
 
 export default App;
+ 
